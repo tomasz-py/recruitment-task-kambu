c2210b8ad1ecbbcbd689b7f0869428c5365cc379
Tomasz-py <tomasz.pyclik@gmail.com>
2019-06-05 15:26:21 +0200

    Updates

---
+++ precache-manifest.b4d1fb76b63fd7a3bf7bcc248f1fc57c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b189409f85186787bfbc05dd0e13f856",
+    "url": "/recruitment-task-kambu/index.html"
+  },
+  {
+    "revision": "0fb5a67b68f5f6bb2a9e",
+    "url": "/recruitment-task-kambu/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7c32a1c5b668e4b1ad78",
+    "url": "/recruitment-task-kambu/static/js/2.60170c36.chunk.js"
+  },
+  {
+    "revision": "0fb5a67b68f5f6bb2a9e",
+    "url": "/recruitment-task-kambu/static/js/main.2d8dfd29.chunk.js"
+  },
+  {
+    "revision": "3ed6f0edc2c2520da76d",
+    "url": "/recruitment-task-kambu/static/js/runtime~main.1b585888.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9d2001106dbbd2c9ea0ee2d4258bb85.js"
+  "/recruitment-task-kambu/precache-manifest.b4d1fb76b63fd7a3bf7bcc248f1fc57c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recruitment-task-kambu/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
