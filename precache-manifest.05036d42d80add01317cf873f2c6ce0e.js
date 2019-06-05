857f92b36d6eb5ed3948d8c9f481693f08e76a01
Tomasz-py <tomasz.pyclik@gmail.com>
2019-06-05 15:11:06 +0200

    Updates

---
+++ precache-manifest.05036d42d80add01317cf873f2c6ce0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89dda447cff35704678ba53d6c05464f",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "ff033baab201794fbaea",
+    "url": "/currency/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7c32a1c5b668e4b1ad78",
+    "url": "/currency/static/js/2.60170c36.chunk.js"
+  },
+  {
+    "revision": "ff033baab201794fbaea",
+    "url": "/currency/static/js/main.2358ac2c.chunk.js"
+  },
+  {
+    "revision": "682945edc1532981b2df",
+    "url": "/currency/static/js/runtime~main.c13dde9a.js"
+  }
+]);
